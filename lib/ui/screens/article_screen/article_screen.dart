f7c66befcbbaa9dd764ccd8929c0558a42dcb849
komugi8 <8448eito@gmail.com>
2023-10-01 01:24:57 +0900

    feat: 3種の初期スクリーンを追加

---
+++ lib/ui/screens/article_screen/article_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ArticleScreen extends StatelessWidget {
+  const ArticleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('article'),
+      ),
+    );
+  }
+}
