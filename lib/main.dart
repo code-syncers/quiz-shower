af49bb5dc87cc82943bd3f55ca3c3db3f3a9f65c
komugi8 <8448eito@gmail.com>
2023-10-01 12:59:32 +0900

    chore: formatの修正とtitleの変更

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class QuizShower extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quiz Shower',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
