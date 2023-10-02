a6599a79e38aaec040da707856d144077c6f3ebf
komugi8 <8448eito@gmail.com>
2023-10-02 11:00:42 +0900

    chore: クラス名の変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_shower/ui/navigation/quiz_shower_navigation.dart';
+import 'package:quiz_shower/quiz_shower_app.dart';
 
 void main() {
-  runApp(const QuizShower());
-}
-
-class QuizShower extends StatelessWidget {
-  const QuizShower({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Quiz Shower',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const QuizShowerNavigation(),
-    );
-  }
+  runApp(const QuizShowerApp());
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quiz_shower/main.dart';
+import 'package:quiz_shower/quiz_shower_app.dart';
 
 void main() {
   testWidgets('Smoke test', (tester) async {
-    await tester.pumpWidget(const QuizShower());
+    await tester.pumpWidget(const QuizShowerApp());
   });
 }
