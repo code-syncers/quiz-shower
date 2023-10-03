911b9a598802a315ac7d976e6c51203338e41896
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-03 15:52:25 +0900

    chore: Body部分をスクロール可能にする

--- lib/ui/screen/article_screen/article_screen.dart
+++ lib/ui/screen/article_screen/article_screen.dart
@@ -6,12 +6,13 @@ class ArticleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('記事を入力します'),
-      ),
-      body: const Center(
-        child: Text('article'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('記事を入力します'),
+        ),
+        body: const SingleChildScrollView(
+          child: Center(
+            child: Text('article'),
+          ),
+        ));
   }
 }
