b48237ad6d204bf0dd08bb3d8deb3fd10638db44
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-03 18:21:54 +0900

    chore: Bodyに「正答率(最新)」「習得状況」「復習する」の3つの見出しをつける

--- lib/ui/screen/user_screen/user_screen.dart
+++ lib/ui/screen/user_screen/user_screen.dart
@@ -10,9 +10,17 @@ class UserScreen extends StatelessWidget {
           title: const Text('記事のタイトル'),
         ),
         body: const SingleChildScrollView(
-          child: Center(
-            child: Text('user'),
-          ),
-        ));
+            child: Padding(
+          padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text('正答率(最新)'),
+            SizedBox(height: 20),
+            Text('習得状況'),
+            SizedBox(height: 20),
+            Text('復習する'),
+            SizedBox(height: 20),
+          ]),
+        )));
   }
 }
