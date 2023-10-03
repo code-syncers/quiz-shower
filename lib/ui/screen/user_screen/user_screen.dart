f04042381f8beca3a6917e5a76cedae7198102d6
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-03 17:58:32 +0900

    chore: AppBarとBodyを作成する

--- lib/ui/screen/user_screen/user_screen.dart
+++ lib/ui/screen/user_screen/user_screen.dart
@@ -5,8 +5,11 @@ class UserScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('記事のタイトル'),
+      ),
+      body: const Center(
         child: Text('user'),
       ),
     );
