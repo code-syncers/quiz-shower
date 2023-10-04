3e62a896ae9401bc3e1485409b5386369d0b6615
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-04 17:27:15 +0900

    feat: 中華フォントを解消

--- lib/ui/screen/article_screen/article_screen.dart
+++ lib/ui/screen/article_screen/article_screen.dart
@@ -7,7 +7,10 @@ class ArticleScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('article'),
+        child: Text(
+          '直る、与える、社会、0123456789,池尾勇磨',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
