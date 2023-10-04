90901e7430e983bf1acd22b6dc4eb089e7432389
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-04 17:29:20 +0900

    chore: 不要な変更の取り消し

--- lib/ui/screen/article_screen/article_screen.dart
+++ lib/ui/screen/article_screen/article_screen.dart
@@ -7,10 +7,7 @@ class ArticleScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text(
-          '直る、与える、社会、0123456789,池尾勇磨',
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
+        child: Text('article'),
       ),
     );
   }
