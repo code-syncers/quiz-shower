2c835b2c14cc86525a5572b875de62d215c668cd
Yuma Ikeo <115936714+yumayuma708@users.noreply.github.com>
2023-10-07 23:56:05 +0900

    feat: ログイン機能の搭載 (#37)
    * feat: Firebaseパッケージの導入、SignInScreenに移動するように設定
    
    * feat: ログインしているユーザーが表示されるように設定、ログインした時にSnackBarが表示されるように設定
    
    * feat: ログアウト機能の実装
    
    * feat: ToDo  Firebaseにログインできるように設定  ログインしたら、ホーム画面に戻ってくるように設定  ログインしました、のSnackBarを表示  ログインしているユーザーのメールアドレスが表示されるように設定  ログアウト機能の実装  ログインしていない時はログインしていませんと表示されるように設定  ログアウト、ログインボタンの表示を状態に応じて変更する
    
    * feat: ログインしたらSnackBarを表示して元の画面に戻るように設定
    
    * test: add setupFirebaseAuthMocks to initialize FirebaseAuth
    
    * chore(deps): add firebase_core_platform_interface
    
    * refactor: organize import
    
    * refactor: fix lint issues
    
    * refactor: dart format
    
    ---------
    
    Co-authored-by: keigomichi <k.keigomichi@gmail.com>

--- lib/ui/widget/quiz_shower_scaffold.dart
+++ lib/ui/widget/quiz_shower_scaffold.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_shower/ui/screen/article_screen/article_screen.dart';
 import 'package:quiz_shower/ui/screen/quiz_screen/quiz_screen.dart';
@@ -12,6 +14,8 @@ class QuizShowerScaffold extends StatefulWidget {
 
 class _QuizShowerScaffoldState extends State<QuizShowerScaffold> {
   var _currentPageIndex = 0;
+  String _currentUserEmail = '';
+  bool _isUserLoggedIn = false;
 
   final _pages = <Widget>[
     const ArticleScreen(),
@@ -19,69 +23,185 @@ class _QuizShowerScaffoldState extends State<QuizShowerScaffold> {
     const UserScreen(),
   ];
 
+  final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      if (user != null && !user.isAnonymous && !_isUserLoggedIn) {
+        _isUserLoggedIn = true;
+        setState(() {
+          _currentUserEmail = user.email ?? '';
+        });
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          _scaffoldMessengerKey.currentState?.showSnackBar(
+            const SnackBar(
+              content: Text('ログインしました'),
+              duration: Duration(seconds: 2),
+            ),
+          );
+        });
+      } else if (user == null && _isUserLoggedIn) {
+        _isUserLoggedIn = false;
+        setState(() {
+          _currentUserEmail = '';
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () => Scaffold.of(context).openDrawer(),
+    return ScaffoldMessenger(
+      key: _scaffoldMessengerKey,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Builder(
+            builder: (context) => IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () => Scaffold.of(context).openDrawer(),
+            ),
           ),
         ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                child: Text(
+                  _currentUserEmail.isEmpty ? 'ログインしていません' : _currentUserEmail,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                    fontSize: 24,
+                  ),
+                ),
               ),
-              child: const Text('Menu'),
+              ListTile(
+                title: const Text('設定'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/setting_screen');
+                },
+              ),
+              if (!_isUserLoggedIn)
+                ListTile(
+                  title: const Text('ログイン/登録'),
+                  onTap: () async {
+                    Navigator.pop(context); // close the drawer
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignInScreen(
+                          actions: [
+                            AuthStateChangeAction<SignedIn>((context, state) {
+                              _scaffoldMessengerKey.currentState?.showSnackBar(
+                                const SnackBar(
+                                  content: Text('ログインしました'),
+                                  duration: Duration(seconds: 2),
+                                ),
+                              );
+                              if (!mounted) return;
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const QuizShowerScaffold(),
+                                ),
+                              );
+                            }),
+                          ],
+                          providers: [
+                            EmailAuthProvider(),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              if (_isUserLoggedIn)
+                ListTile(
+                  title: const Text('ログアウト'),
+                  onTap: () async {
+                    await showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text(
+                            'ログアウト',
+                            textAlign: TextAlign.center,
+                          ),
+                          content: const Text(
+                            '本当にログアウトしますか？',
+                            textAlign: TextAlign.center,
+                          ),
+                          actions: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Spacer(
+                                  flex: 3,
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('キャンセル'),
+                                ),
+                                const Spacer(
+                                  flex: 1,
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    Navigator.of(context).pop();
+                                    await FirebaseAuth.instance.signOut();
+                                  },
+                                  child: const Text('ログアウト'),
+                                ),
+                                const Spacer(
+                                  flex: 3,
+                                ),
+                              ],
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+            ],
+          ),
+        ),
+        body: _pages[_currentPageIndex],
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              _currentPageIndex = index;
+            });
+          },
+          selectedIndex: _currentPageIndex,
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+          destinations: const <Widget>[
+            NavigationDestination(
+              icon: Icon(Icons.description_outlined),
+              selectedIcon: Icon(Icons.description_rounded),
+              label: '記事',
             ),
-            ListTile(
-              title: const Text('設定'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/setting_screen');
-              },
+            NavigationDestination(
+              icon: Icon(Icons.quiz_outlined),
+              selectedIcon: Icon(Icons.quiz_rounded),
+              label: 'クイズ',
             ),
-            ListTile(
-              title: const Text('サインイン'),
-              onTap: () {
-                Navigator.pop(context);
-              },
+            NavigationDestination(
+              icon: Icon(Icons.account_circle_outlined),
+              selectedIcon: Icon(Icons.account_circle_rounded),
+              label: 'ユーザー',
             ),
           ],
         ),
-      ),
-      body: _pages[_currentPageIndex],
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            _currentPageIndex = index;
-          });
-        },
-        selectedIndex: _currentPageIndex,
-        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-        destinations: const <Widget>[
-          NavigationDestination(
-            icon: Icon(Icons.description_outlined),
-            selectedIcon: Icon(Icons.description_rounded),
-            label: '記事',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.quiz_outlined),
-            selectedIcon: Icon(Icons.quiz_rounded),
-            label: 'クイズ',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.account_circle_outlined),
-            selectedIcon: Icon(Icons.account_circle_rounded),
-            label: 'ユーザー',
-          ),
-        ],
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:quiz_shower/quiz_shower_app.dart';
 
+import 'mock.dart';
+
 void main() {
+  setupFirebaseAuthMocks();
+
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
+
   testWidgets('Smoke test', (tester) async {
     await tester.pumpWidget(const QuizShowerApp());
   });
