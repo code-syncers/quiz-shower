2487fa792fa349157badf6ed21bf05551e7e18b2
keigomichi <k.keigomichi@gmail.com>
2023-10-07 23:23:04 +0900

    chore: setup custom lint rules (#40)
    * chore: setup custom lint rules
    
    * refactor: fix lint issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
-import 'package:quiz_shower/quiz_shower_app.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:logger/logger.dart';
+import 'package:quiz_shower/firebase_options.dart';
+import 'package:quiz_shower/quiz_shower_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
