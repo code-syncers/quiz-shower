22c5d625861366cb837e16ceddfe5340bcc45afb
komugi8 <8448eito@gmail.com>
2023-10-09 00:09:48 +0900

    feat: add article model

--- lib/data/model/article.freezed.dart
+++ lib/data/model/article.freezed.dart
@@ -16,10 +16,17 @@ final _privateConstructorUsedError = UnsupportedError(
 
 /// @nodoc
 mixin _$Article {
+  String get id => throw _privateConstructorUsedError;
+  String get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdAt => throw _privateConstructorUsedError;
   String get title => throw _privateConstructorUsedError;
   String get description => throw _privateConstructorUsedError;
+  bool get isBookmarked => throw _privateConstructorUsedError;
+  ArticleType get type => throw _privateConstructorUsedError;
+  String? get url => throw _privateConstructorUsedError;
   String get content => throw _privateConstructorUsedError;
-  String get url => throw _privateConstructorUsedError;
+  List<Quiz> get quizzes => throw _privateConstructorUsedError;
+  bool get isPublic => throw _privateConstructorUsedError;
 
   @JsonKey(ignore: true)
   $ArticleCopyWith<Article> get copyWith => throw _privateConstructorUsedError;
@@ -30,7 +37,18 @@ abstract class $ArticleCopyWith<$Res> {
   factory $ArticleCopyWith(Article value, $Res Function(Article) then) =
       _$ArticleCopyWithImpl<$Res, Article>;
   @useResult
-  $Res call({String title, String description, String content, String url});
+  $Res call(
+      {String id,
+      String createdBy,
+      DateTime createdAt,
+      String title,
+      String description,
+      bool isBookmarked,
+      ArticleType type,
+      String? url,
+      String content,
+      List<Quiz> quizzes,
+      bool isPublic});
 }
 
 /// @nodoc
@@ -46,12 +64,31 @@ class _$ArticleCopyWithImpl<$Res, $Val extends Article>
   @pragma('vm:prefer-inline')
   @override
   $Res call({
+    Object? id = null,
+    Object? createdBy = null,
+    Object? createdAt = null,
     Object? title = null,
     Object? description = null,
+    Object? isBookmarked = null,
+    Object? type = null,
+    Object? url = freezed,
     Object? content = null,
-    Object? url = null,
+    Object? quizzes = null,
+    Object? isPublic = null,
   }) {
     return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdBy: null == createdBy
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
       title: null == title
           ? _value.title
           : title // ignore: cast_nullable_to_non_nullable
@@ -60,14 +97,30 @@ class _$ArticleCopyWithImpl<$Res, $Val extends Article>
           ? _value.description
           : description // ignore: cast_nullable_to_non_nullable
               as String,
+      isBookmarked: null == isBookmarked
+          ? _value.isBookmarked
+          : isBookmarked // ignore: cast_nullable_to_non_nullable
+              as bool,
+      type: null == type
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as ArticleType,
+      url: freezed == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as String?,
       content: null == content
           ? _value.content
           : content // ignore: cast_nullable_to_non_nullable
               as String,
-      url: null == url
-          ? _value.url
-          : url // ignore: cast_nullable_to_non_nullable
-              as String,
+      quizzes: null == quizzes
+          ? _value.quizzes
+          : quizzes // ignore: cast_nullable_to_non_nullable
+              as List<Quiz>,
+      isPublic: null == isPublic
+          ? _value.isPublic
+          : isPublic // ignore: cast_nullable_to_non_nullable
+              as bool,
     ) as $Val);
   }
 }
@@ -79,7 +132,18 @@ abstract class _$$ArticleImplCopyWith<$Res> implements $ArticleCopyWith<$Res> {
       __$$ArticleImplCopyWithImpl<$Res>;
   @override
   @useResult
-  $Res call({String title, String description, String content, String url});
+  $Res call(
+      {String id,
+      String createdBy,
+      DateTime createdAt,
+      String title,
+      String description,
+      bool isBookmarked,
+      ArticleType type,
+      String? url,
+      String content,
+      List<Quiz> quizzes,
+      bool isPublic});
 }
 
 /// @nodoc
@@ -93,12 +157,31 @@ class __$$ArticleImplCopyWithImpl<$Res>
   @pragma('vm:prefer-inline')
   @override
   $Res call({
+    Object? id = null,
+    Object? createdBy = null,
+    Object? createdAt = null,
     Object? title = null,
     Object? description = null,
+    Object? isBookmarked = null,
+    Object? type = null,
+    Object? url = freezed,
     Object? content = null,
-    Object? url = null,
+    Object? quizzes = null,
+    Object? isPublic = null,
   }) {
     return _then(_$ArticleImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdBy: null == createdBy
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
       title: null == title
           ? _value.title
           : title // ignore: cast_nullable_to_non_nullable
@@ -107,14 +190,30 @@ class __$$ArticleImplCopyWithImpl<$Res>
           ? _value.description
           : description // ignore: cast_nullable_to_non_nullable
               as String,
+      isBookmarked: null == isBookmarked
+          ? _value.isBookmarked
+          : isBookmarked // ignore: cast_nullable_to_non_nullable
+              as bool,
+      type: null == type
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as ArticleType,
+      url: freezed == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as String?,
       content: null == content
           ? _value.content
           : content // ignore: cast_nullable_to_non_nullable
               as String,
-      url: null == url
-          ? _value.url
-          : url // ignore: cast_nullable_to_non_nullable
-              as String,
+      quizzes: null == quizzes
+          ? _value._quizzes
+          : quizzes // ignore: cast_nullable_to_non_nullable
+              as List<Quiz>,
+      isPublic: null == isPublic
+          ? _value.isPublic
+          : isPublic // ignore: cast_nullable_to_non_nullable
+              as bool,
     ));
   }
 }
@@ -123,24 +222,53 @@ class __$$ArticleImplCopyWithImpl<$Res>
 
 class _$ArticleImpl extends _Article {
   const _$ArticleImpl(
-      {required this.title,
+      {required this.id,
+      required this.createdBy,
+      required this.createdAt,
+      required this.title,
       required this.description,
+      required this.isBookmarked,
+      required this.type,
+      required this.url,
       required this.content,
-      required this.url})
-      : super._();
+      required final List<Quiz> quizzes,
+      this.isPublic = false})
+      : _quizzes = quizzes,
+        super._();
 
+  @override
+  final String id;
+  @override
+  final String createdBy;
+  @override
+  final DateTime createdAt;
   @override
   final String title;
   @override
   final String description;
   @override
-  final String content;
+  final bool isBookmarked;
   @override
-  final String url;
+  final ArticleType type;
+  @override
+  final String? url;
+  @override
+  final String content;
+  final List<Quiz> _quizzes;
+  @override
+  List<Quiz> get quizzes {
+    if (_quizzes is EqualUnmodifiableListView) return _quizzes;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_quizzes);
+  }
+
+  @override
+  @JsonKey()
+  final bool isPublic;
 
   @override
   String toString() {
-    return 'Article(title: $title, description: $description, content: $content, url: $url)';
+    return 'Article(id: $id, createdBy: $createdBy, createdAt: $createdAt, title: $title, description: $description, isBookmarked: $isBookmarked, type: $type, url: $url, content: $content, quizzes: $quizzes, isPublic: $isPublic)';
   }
 
   @override
@@ -148,16 +276,38 @@ class _$ArticleImpl extends _Article {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$ArticleImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.createdBy, createdBy) ||
+                other.createdBy == createdBy) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
             (identical(other.title, title) || other.title == title) &&
             (identical(other.description, description) ||
                 other.description == description) &&
+            (identical(other.isBookmarked, isBookmarked) ||
+                other.isBookmarked == isBookmarked) &&
+            (identical(other.type, type) || other.type == type) &&
+            (identical(other.url, url) || other.url == url) &&
             (identical(other.content, content) || other.content == content) &&
-            (identical(other.url, url) || other.url == url));
+            const DeepCollectionEquality().equals(other._quizzes, _quizzes) &&
+            (identical(other.isPublic, isPublic) ||
+                other.isPublic == isPublic));
   }
 
   @override
-  int get hashCode =>
-      Object.hash(runtimeType, title, description, content, url);
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      createdBy,
+      createdAt,
+      title,
+      description,
+      isBookmarked,
+      type,
+      url,
+      content,
+      const DeepCollectionEquality().hash(_quizzes),
+      isPublic);
 
   @JsonKey(ignore: true)
   @override
@@ -168,20 +318,41 @@ class _$ArticleImpl extends _Article {
 
 abstract class _Article extends Article {
   const factory _Article(
-      {required final String title,
+      {required final String id,
+      required final String createdBy,
+      required final DateTime createdAt,
+      required final String title,
       required final String description,
+      required final bool isBookmarked,
+      required final ArticleType type,
+      required final String? url,
       required final String content,
-      required final String url}) = _$ArticleImpl;
+      required final List<Quiz> quizzes,
+      final bool isPublic}) = _$ArticleImpl;
   const _Article._() : super._();
 
+  @override
+  String get id;
+  @override
+  String get createdBy;
+  @override
+  DateTime get createdAt;
   @override
   String get title;
   @override
   String get description;
   @override
+  bool get isBookmarked;
+  @override
+  ArticleType get type;
+  @override
+  String? get url;
+  @override
   String get content;
   @override
-  String get url;
+  List<Quiz> get quizzes;
+  @override
+  bool get isPublic;
   @override
   @JsonKey(ignore: true)
   _$$ArticleImplCopyWith<_$ArticleImpl> get copyWith =>
