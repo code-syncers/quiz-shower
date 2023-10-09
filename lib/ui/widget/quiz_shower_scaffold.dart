efdc2d975dac32c76f986bdbf4a953d3498b90be
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-09 18:42:30 +0900

    fix: 新規登録完了時にトップページに遷移

--- lib/ui/widget/quiz_shower_scaffold.dart
+++ lib/ui/widget/quiz_shower_scaffold.dart
@@ -113,6 +113,22 @@ class _QuizShowerScaffoldState extends State<QuizShowerScaffold> {
                                 ),
                               );
                             }),
+                            AuthStateChangeAction<UserCreated>(
+                                (context, state) {
+                              _scaffoldMessengerKey.currentState?.showSnackBar(
+                                const SnackBar(
+                                  content: Text('ログインしました'),
+                                  duration: Duration(seconds: 2),
+                                ),
+                              );
+                              if (!mounted) return;
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const QuizShowerScaffold(),
+                                ),
+                              );
+                            }),
                           ],
                           providers: [
                             EmailAuthProvider(),
