38f5c7a34645fb3e18e270d272254e02644145d6
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-10 01:44:27 +0900

    feat: ユーザー名、ユーザーID、編集ボタン、アイコンの表示場所の作成

--- lib/ui/screen/setting_screen/setting_screen.dart
+++ lib/ui/screen/setting_screen/setting_screen.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
 
-class SettingScreen extends StatelessWidget {
-  const SettingScreen({super.key});
+class SettingScreen extends StatefulWidget {
+  const SettingScreen({Key? key}) : super(key: key);
 
   @override
+  SettingScreenState createState() => SettingScreenState();
+}
+
+class SettingScreenState extends State<SettingScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('settings'),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.account_circle,
+                size: 80,
+              ),
+              const SizedBox(
+                width: 30,
+              ),
+              const Column(
+                children: [
+                  Text('ユーザー名', style: TextStyle(fontSize: 20)),
+                  Text(
+                    'ユーザーID',
+                    style: TextStyle(fontSize: 16),
+                  ), // ユーザーIDを追加
+                ],
+              ),
+              const SizedBox(
+                width: 30,
+              ),
+              IconButton(
+                icon: const Icon(Icons.edit),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
