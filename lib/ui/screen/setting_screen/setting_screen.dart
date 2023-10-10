60b51d6b3bc267b292e548ece8d0cad63048d197
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-10 19:51:55 +0900

    feat: ダークモードと、モード切り替え用のUIを作る

--- lib/ui/screen/setting_screen/setting_screen.dart
+++ lib/ui/screen/setting_screen/setting_screen.dart
@@ -12,40 +12,106 @@ class SettingScreenState extends State<SettingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.account_circle,
-                size: 80,
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Spacer(
+                  flex: 2,
+                ),
+                const Icon(
+                  Icons.account_circle,
+                  size: 80,
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('ユーザー名', style: TextStyle(fontSize: 20)),
+                    Text(
+                      'ユーザーID',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                const Spacer(
+                  flex: 3,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () {},
+                ),
+                const Spacer(
+                  flex: 2,
+                ),
+              ],
+            ),
+            const Divider(
+              thickness: 1,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const Text(
+              'アプリの設定',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(
-                width: 30,
-              ),
-              const Column(
-                children: [
-                  Text('ユーザー名', style: TextStyle(fontSize: 20)),
-                  Text(
-                    'ユーザーID',
-                    style: TextStyle(fontSize: 16),
-                  ), // ユーザーIDを追加
-                ],
-              ),
-              const SizedBox(
-                width: 30,
-              ),
-              IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ],
+              textAlign: TextAlign.left,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const Row(
+              children: [
+                Icon(
+                  Icons.dark_mode,
+                  size: 30,
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Text(
+                  'アプリのテーマ',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                Spacer(),
+                Text('現在のテーマ'),
+              ],
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const Row(
+              children: [
+                Icon(
+                  Icons.settings,
+                  size: 30,
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Text(
+                  '表示形式',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                Spacer(),
+                Text('インプットorアウトプット'),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
