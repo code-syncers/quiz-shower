950bd10c27bf91d931d876721b2f8469c2562781
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-11 13:54:10 +0900

    feat: アカウントの設定項目を追加

--- lib/ui/screen/setting_screen/setting_screen.dart
+++ lib/ui/screen/setting_screen/setting_screen.dart
@@ -20,64 +20,36 @@ class SettingScreenState extends State<SettingScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Spacer(
-                  flex: 2,
-                ),
-                const Icon(
-                  Icons.account_circle,
-                  size: 80,
-                ),
-                const Spacer(
-                  flex: 1,
-                ),
+                const Spacer(flex: 2),
+                const Icon(Icons.account_circle_rounded, size: 80),
+                const Spacer(flex: 1),
                 const Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('ユーザー名', style: TextStyle(fontSize: 20)),
-                    Text(
-                      'ユーザーID',
-                      style: TextStyle(fontSize: 16),
-                    ),
+                    Text('表示名', style: TextStyle(fontSize: 20)),
+                    Text('ユーザーID', style: TextStyle(fontSize: 16)),
                   ],
                 ),
-                const Spacer(
-                  flex: 3,
-                ),
+                const Spacer(flex: 3),
                 IconButton(
-                  icon: const Icon(Icons.edit),
+                  icon: const Icon(Icons.edit_rounded),
                   onPressed: () {},
                 ),
-                const Spacer(
-                  flex: 2,
-                ),
+                const Spacer(flex: 2),
               ],
             ),
-            const Divider(
-              thickness: 1,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
+            const Divider(thickness: 1),
+            const SizedBox(height: 16),
             const Text(
               'アプリの設定',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               textAlign: TextAlign.left,
             ),
-            const SizedBox(
-              height: 16,
-            ),
+            const SizedBox(height: 16),
             const Row(
               children: [
-                Icon(
-                  Icons.dark_mode,
-                  size: 30,
-                ),
-                SizedBox(
-                  width: 16,
-                ),
+                Icon(Icons.dark_mode_rounded, size: 28),
+                SizedBox(width: 16),
                 Text(
                   'アプリのテーマ',
                   style: TextStyle(
@@ -88,26 +60,114 @@ class SettingScreenState extends State<SettingScreen> {
                 Text('現在のテーマ'),
               ],
             ),
-            const SizedBox(
-              height: 16,
-            ),
+            const SizedBox(height: 16),
             const Row(
               children: [
-                Icon(
-                  Icons.settings,
-                  size: 30,
-                ),
-                SizedBox(
-                  width: 16,
-                ),
+                Icon(Icons.settings_rounded, size: 28),
+                SizedBox(width: 16),
                 Text(
                   '表示形式',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
+                  style: TextStyle(fontSize: 16),
                 ),
                 Spacer(),
-                Text('インプットorアウトプット'),
+                Text('記事orクイズ'),
+              ],
+            ),
+            const SizedBox(height: 16),
+            const Row(
+              children: [
+                Icon(Icons.notifications_rounded, size: 28),
+                SizedBox(width: 16),
+                Text(
+                  '通知の設定',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Spacer(),
+                Text('現在の通知設定'),
+              ],
+            ),
+            const SizedBox(height: 16),
+            const Divider(thickness: 1),
+            const SizedBox(height: 16),
+            const Text(
+              'アカウントの設定',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.left,
+            ),
+            const SizedBox(height: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.mail_rounded, size: 28),
+                    SizedBox(width: 16),
+                    Text(
+                      'メールアドレスを変更',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Spacer(),
+                    Icon(Icons.navigate_next_rounded, size: 24),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.lock_rounded, size: 28),
+                    SizedBox(width: 16),
+                    Text(
+                      'パスワードを変更',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Spacer(),
+                    Icon(Icons.navigate_next_rounded, size: 24),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.verified_user_rounded, size: 30),
+                    SizedBox(width: 16),
+                    Text('認証方法を追加', style: TextStyle(fontSize: 16)),
+                    Spacer(),
+                    Icon(Icons.navigate_next_rounded, size: 24),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.logout_rounded, size: 30),
+                    SizedBox(width: 16),
+                    Text('ログアウト', style: TextStyle(fontSize: 16)),
+                    Spacer(),
+                    Icon(Icons.navigate_next_rounded, size: 24),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.person_off_rounded,
+                      size: 28,
+                      color: Theme.of(context).colorScheme.error,
+                    ),
+                    const SizedBox(width: 16),
+                    Text(
+                      'アカウントを削除',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Theme.of(context).colorScheme.error, // ここで色を変更
+                      ),
+                    ),
+                    const Spacer(),
+                    const Icon(Icons.navigate_next_rounded, size: 24),
+                  ],
+                ),
               ],
             ),
           ],
