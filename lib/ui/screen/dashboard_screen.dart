658fb373342ccc7dd77ec6b1f78249288ddd21ff
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-13 21:32:51 +0900

    feat: 見出しをつける

--- lib/ui/screen/dashboard_screen.dart
+++ lib/ui/screen/dashboard_screen.dart
@@ -7,10 +7,43 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.chevron_left),
+          onPressed: () {
+            Navigator.of(context).pop(); // 1つ目の画面を戻る
+            Navigator.of(context).pop(); // 2つ目の画面を戻る
+          },
+        ),
         title: const Text('ダッシュボード'),
       ),
-      body: const Center(
-        child: Text('dashboard'),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'プロフィール',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 16),
+              Text(
+                '履歴',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 16),
+              Text(
+                '復習する',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 16),
+              Text(
+                '通知',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
