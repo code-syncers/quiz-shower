c55c9806bd605f2b053d544a5f3fac5193e6e70c
notchcoder <kokiuaxdx@gmail.com>
2023-10-14 02:59:36 +0900

    refactor: QuizListの要素単位の繰り返し処理を実装

--- lib/ui/screen/dashboard_screen.dart
+++ lib/ui/screen/dashboard_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_shower/data/model/quiz.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final quizList = [Quiz.mock()];
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -132,73 +134,14 @@ class DashboardScreen extends StatelessWidget {
                 '復習する',
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              ListTile(
-                title: const Row(
-                  children: [
-                    Text(
-                      'Q21',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 8),
-                    Text('透明UIを作る'),
-                  ],
-                ),
-                subtitle: const Text('透明UIについて説明したものを次のうちから選べ'),
-                trailing: const Icon(Icons.navigate_next),
-                onTap: () {},
-              ),
-              const SizedBox(height: 8),
-              ListTile(
-                title: const Row(
-                  children: [
-                    Text(
-                      'Q21',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 8),
-                    Text('透明UIを作る'),
-                  ],
-                ),
-                subtitle: const Text('透明UIについて説明したものを次のうちから選べ'),
-                trailing: const Icon(Icons.navigate_next),
-                onTap: () {},
-              ),
-              const SizedBox(height: 8),
-              ListTile(
-                title: const Row(
-                  children: [
-                    Text(
-                      'Q21',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 8),
-                    Text('透明UIを作る'),
-                  ],
-                ),
-                subtitle: const Text('透明UIについて説明したものを次のうちから選べ'),
-                trailing: const Icon(Icons.navigate_next),
-                onTap: () {},
-              ),
-              const SizedBox(height: 8),
-              ListTile(
-                title: const Row(
-                  children: [
-                    Text(
-                      'Q21',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 8),
-                    Text('透明UIを作る'),
-                  ],
-                ),
-                subtitle: const Text('透明UIについて説明したものを次のうちから選べ'),
-                trailing: const Icon(Icons.navigate_next),
-                onTap: () {},
-              ),
+              ...quizList.map((element) {
+                return ListTile(
+                  title: Text(element.title),
+                  subtitle: Text(element.statement),
+                  trailing: const Icon(Icons.navigate_next),
+                  onTap: () {},
+                );
+              }).toList(),
               const SizedBox(height: 8),
               const Divider(thickness: 1),
               const SizedBox(height: 8),
