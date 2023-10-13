c0c71156ea37b499f64fb812bcfcbea00f65358d
yumayuma708 <yumaskywalker708@gmail.com>
2023-10-13 23:45:41 +0900

    feat: プロフィール、履歴の内容を作成

--- lib/ui/screen/dashboard_screen.dart
+++ lib/ui/screen/dashboard_screen.dart
@@ -7,14 +7,10 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.chevron_left),
-          onPressed: () {
-            Navigator.of(context).pop(); // 1つ目の画面を戻る
-            Navigator.of(context).pop(); // 2つ目の画面を戻る
-          },
+        title: const Text(
+          'ダッシュボード',
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
-        title: const Text('ダッシュボード'),
       ),
       body: SingleChildScrollView(
         child: Container(
@@ -22,25 +18,125 @@ class DashboardScreen extends StatelessWidget {
           child: const Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'プロフィール',
-                style: TextStyle(fontSize: 20),
+              Row(
+                children: [
+                  Text(
+                    'プロフィール',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(width: 16),
+                  Icon(
+                    Icons.edit_rounded,
+                    size: 20,
+                  ),
+                ],
+              ),
+              Divider(thickness: 1),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  SizedBox(width: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '表示名：ゆうま(例)',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'exp：100(例)',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.account_circle_rounded,
+                    size: 60,
+                  ),
+                ],
               ),
               SizedBox(height: 16),
-              Text(
-                '履歴',
-                style: TextStyle(fontSize: 20),
+              Row(
+                children: [
+                  Text(
+                    '履歴：',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(width: 16),
+                  Text(
+                    '記事名(例)',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(width: 16),
+                  Icon(
+                    Icons.edit_rounded,
+                    size: 20,
+                  ),
+                ],
+              ),
+              Divider(thickness: 1),
+              Column(
+                children: [
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.check_box_outline_blank_rounded,
+                        size: 20,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        '正答率',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 12),
+                  Row(
+                    children: [
+                      SizedBox(width: 24),
+                      Text('○/○○ 問正解(例)'),
+                      Spacer(),
+                      Text('ここにグラフを表示させる'),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.check_box_outline_blank_rounded,
+                        size: 20,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        '対戦結果',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 12),
+                  Row(
+                    children: [
+                      SizedBox(width: 24),
+                      Text('対戦結果を表示させる'),
+                    ],
+                  ),
+                ],
               ),
               SizedBox(height: 16),
               Text(
                 '復習する',
                 style: TextStyle(fontSize: 20),
               ),
+              Divider(thickness: 1),
               SizedBox(height: 16),
               Text(
                 '通知',
                 style: TextStyle(fontSize: 20),
               ),
+              Divider(thickness: 1),
             ],
           ),
         ),
