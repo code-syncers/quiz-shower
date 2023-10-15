2a6f0a3ed31234e694ed872085976475918c481d
komugi8 <120884254+komugi8@users.noreply.github.com>
2023-10-15 13:51:20 +0900

    feat: 記事追加画面にURL入力フィールドの追加とfirestoreへのデータ渡し (#69)

--- lib/ui/screen/home_screen/add_article_screen.dart
+++ lib/ui/screen/home_screen/add_article_screen.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:quiz_shower/data/model/article.dart';
+import 'package:quiz_shower/data/repository/di/article_repository_provider.dart';
+import 'package:uuid/uuid.dart';
 
-class AddArticleScreen extends StatelessWidget {
+class AddArticleScreen extends HookConsumerWidget {
   const AddArticleScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final repository = ref.watch(articleRepositoryProvider);
+    final urlController = useTextEditingController();
+    final titleController = useTextEditingController();
+    final contentController = useTextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('記事を追加'),
       ),
       body: Stack(
         children: [
-          const SingleChildScrollView(
+          SingleChildScrollView(
             child: Padding(
-              padding: EdgeInsets.all(24.0),
+              padding: const EdgeInsets.all(24.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
+                  const Text(
+                    '記事のURL',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  TextField(
+                    controller: urlController,
+                    decoration: const InputDecoration(
+                      hintText: 'ここに記事のURLを入力してください',
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  const Text(
                     '記事のタイトル',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
@@ -25,23 +49,25 @@ class AddArticleScreen extends StatelessWidget {
                     ),
                   ),
                   TextField(
-                    decoration: InputDecoration(
+                    controller: titleController,
+                    decoration: const InputDecoration(
                       hintText: 'ここに記事のタイトルを入力してください',
                     ),
                   ),
-                  SizedBox(height: 16.0),
-                  Text(
+                  const SizedBox(height: 16.0),
+                  const Text(
                     '記事の内容',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 18.0,
                     ),
                   ),
-                  SizedBox(height: 16.0),
+                  const SizedBox(height: 16.0),
                   TextField(
+                    controller: contentController,
                     maxLines: null,
                     keyboardType: TextInputType.multiline,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       border: OutlineInputBorder(
                         borderSide: BorderSide(),
                       ),
@@ -57,7 +83,27 @@ class AddArticleScreen extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: FloatingActionButton.extended(
-                onPressed: () {},
+                onPressed: () async {
+                  debugPrint(urlController.text);
+                  debugPrint(titleController.text);
+                  debugPrint(contentController.text);
+                  await repository.createArticle(
+                    Article(
+                      id: const Uuid().v4(),
+                      createdBy: 'hoge',
+                      title: 'title hoge',
+                      description: 'description hoge',
+                      isBookmarked: false,
+                      type: urlController.text != ''
+                          ? ArticleType.url
+                          : ArticleType.manual,
+                      content: urlController.text != ''
+                          ? urlController.text
+                          : contentController.text,
+                      createdAt: DateTime.now(),
+                    ),
+                  );
+                },
                 icon: const Icon(Icons.done),
                 label: const Text('保存'),
               ),
