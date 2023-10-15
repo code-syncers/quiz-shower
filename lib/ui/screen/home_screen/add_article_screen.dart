35850a7507e08165a66a25a7b6c7435b304c9e33
keigomichi <k.keigomichi@gmail.com>
2023-10-15 15:34:18 +0900

    feat: impl article list in home screen (#73)

--- lib/ui/screen/home_screen/add_article_screen.dart
+++ lib/ui/screen/home_screen/add_article_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -87,7 +88,7 @@ class AddArticleScreen extends HookConsumerWidget {
                   await repository.createArticle(
                     Article(
                       id: const Uuid().v4(),
-                      createdBy: 'hoge',
+                      createdBy: FirebaseAuth.instance.currentUser!.uid,
                       title: 'title hoge',
                       description: 'description hoge',
                       isBookmarked: false,
